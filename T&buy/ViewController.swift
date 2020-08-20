3c5529f0f9ebbb77c45cadb24fc77d9aacd4b737
agustiandm <56951536+agustiandm@users.noreply.github.com>
2020-08-20 07:03:54 +0700

    Initial Commit

---
+++ T&buy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  T&buy
+//
+//  Created by Agustian DM on 20/08/20.
+//  Copyright © 2020 Agustian DM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
